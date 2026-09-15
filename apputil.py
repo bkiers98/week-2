a333e8d593fd4c93f523989765bbc0207efad8d9
bkiers98 <167789286+bkiers98@users.noreply.github.com>
2026-09-14 21:32:48 -0400

    Complete exercise 1 and 2.1

--- apputil.py
+++ apputil.py
@@ -4,7 +4,23 @@ import numpy as np
 # update/add code below ...
 
 def ways(n):
-    return None
+    '''
+    This function calculates the number of ways you can make change
+    for a given amount of cents (n) using only pennies and nickels.
+    '''
+
+    #define initial return value and list of options
+    num_ways = 0
+    coin_combos = []
+
+    #iterate through possible solutions, starting with least amount of nickels (0) and going to greatest amount
+    for nickel in range(0, n+1, 5):
+        combo = (int(nickel / 5), n - nickel)
+        coin_combos.append(combo)
+
+    num_ways = len(coin_combos)
+    return (num_ways)
+
 
 def lowest_score(names, scores):
     return None
